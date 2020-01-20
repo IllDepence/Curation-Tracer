dbe329d91d8f87fba4b3453901e3f0c8c7c2c3a6
Tarek Saier <tareksaier@gmail.com>
2020-01-20 17:16:43 +0100

    change response format to ICViewer parsable JSON
    (response in now a curation containing the query
     canvas, annotated with the a list of curation
     backlinks per linked area)

--- curba.py
+++ curba.py
@@ -1,9 +1,12 @@
 import atexit
+import copy
 import json
 import time
 import urllib.parse
+import uuid
 from config import Cfg
 from crawler import crawl
+from collections import OrderedDict
 from flask import Flask, request, abort, jsonify
 from sqlalchemy import text as sqla_text
 from sqlalchemy import create_engine
@@ -23,6 +26,60 @@ atexit.register(lambda: scheduler.shutdown())
 
 app = Flask(__name__)
 
+
+def build_curba_curation(canvas_uri, containing_manifest_uri, backlinks):
+    """ Build a curation containing a single canvas that is annotated with
+        curation backlinks.
+    """
+
+    cur = OrderedDict()
+    cur['@context'] = ['http://iiif.io/api/presentation/2/context.json',
+                       ('http://codh.rois.ac.jp/iiif/curation/1/context.js'
+                        'on')]
+    cur['@type'] = 'cr:Curation'
+    cur['@id'] = 'http://example.org/iiif/curation/{}'.format(uuid.uuid1())
+    cur['viewingHint'] = 'annotation'
+    cur['label'] = 'Curation Backlinks for {}'.format(canvas_uri)
+    cur['selections'] = []
+    sel = OrderedDict()
+    sel['@id'] = 'http://example.org/iiif/range/{}'.format(uuid.uuid1())
+    sel['@type'] = 'sc:Range'
+    sel['label'] = 'Temporary range for displaying a canvas'
+    sel['members'] = []
+    mem = OrderedDict()
+    mem['@id'] = canvas_uri
+    mem['@type'] = 'sc:Canvas'
+    mem['label'] = 'Temporary canvas for displaying annotations'
+    mem['metadata'] = []
+    for xywh, uris in backlinks.items():
+        # For every area
+        mtd = OrderedDict()
+        mtd['label'] = 'Curation Backlinks for {}'.format(canvas_uri)
+        mtd['value'] = []
+        for uri in uris:
+            # For every backlink to a curation
+            ann = OrderedDict()
+            ann['@id'] = 'http://example.org/iiif/annotation/{}'.format(uuid.uuid1())
+            ann['@type'] = 'oa:Annotation'
+            ann['motivation'] = 'sc:painting'
+            ann['resource'] = OrderedDict()
+            ann['resource']['@type'] = 'cnt:ContentAsText'
+            ann['resource']['format'] = 'text/html'
+            ann['resource']['marker'] = OrderedDict()
+            ann['resource']['marker']['border-color'] = '#0f0'
+            ann['resource']['marker']['chars'] = '<a href="{}">Curation</a>'.format(uri)
+            ann['on'] = '{}#xywh={}'.format(canvas_uri, xywh)
+            mtd['value'].append(copy.deepcopy(ann))
+        mem['metadata'].append(copy.deepcopy(mtd))
+    sel['members'].append(copy.deepcopy(mem))
+    sel['within'] = OrderedDict()
+    sel['within']['@id'] = containing_manifest_uri
+    sel['within']['@type'] = 'sc:Manifest'
+    sel['within']['label'] = 'Temporary manifest for displaying a canvas'
+    cur['selections'].append(copy.deepcopy(sel))
+    return cur
+
+
 @app.route('/', methods=['GET'])
 def index():
     canvas_uri_raw = request.args.get('canvas')
@@ -35,20 +92,21 @@ def index():
     db_engine = create_engine(cfg.cfg['db_uri'])
 
     q_can = sqla_text('''
-        SELECT id
+        SELECT id, manifest_jsonld_id
         FROM canvases
         WHERE jsonld_id=:can_uri
         ''')
     print(canvas_uri)
-    can_db_id = db_engine.execute(
+    can_db_tpls = db_engine.execute(
         q_can,
         can_uri=canvas_uri
         ).fetchall()
-    if not can_db_id:
+    if not can_db_tpls:
         return abort(404)  # FIXME not there, respond accordingly
     else:
-        if len(can_db_id) == 1:
-            can_db_id = int(can_db_id[0]['id'])
+        if len(can_db_tpls) == 1:
+            can_db_id = int(can_db_tpls[0]['id'])
+            can_db_man_jsonld_id = can_db_tpls[0]['manifest_jsonld_id']
         else:
             print('multiple canvases w/ same ID (!!!)')  # FIXME problem
 
@@ -74,20 +132,36 @@ def index():
         '''.format(area_query_insert, can_db_id)
     cur_uris = db_engine.execute(q_area).fetchall()
 
-    backlinks = {}
+    backlinks_flat = []
     for row in cur_uris:
         uri = row['uri']
         area = json.loads(row['areajson'])
-        if uri not in backlinks:
-            backlinks[uri] = {'areas':[]}
-        backlinks[uri]['areas'].append(area)
+        backlinks_flat.append([uri, area])
+    # backlinks_by_uri = {}
+    # for bl in backlinks_flat:
+    #     uri, area = bl
+    #     if uri not in backlinks_by_uri:
+    #         backlinks_by_uri[uri] = {'areas':[]}
+    #     backlinks_by_uri[uri]['areas'].append(area)
+    backlinks_by_area = {}
+    for bl in backlinks_flat:
+        uri, area = bl
+        coords = area['coordinates'][0]
+        if not len(coords) == 5:
+            print('unexpected polygon shape (!!!)')  # FIXME problem
+        p1, p2, p3, p4, p5 = coords
+        xywh = '{},{},{},{}'.format(p1[0], p1[1], p2[0]-p1[0], p3[1]-p1[1])
+        if xywh not in backlinks_by_area:
+            backlinks_by_area[xywh] = []
+        backlinks_by_area[xywh].append(uri)
+    display_curation = build_curba_curation(canvas_uri, can_db_man_jsonld_id, backlinks_by_area)
 
-    ret = {
-        'canvas': canvas_uri,
-        'curations_backlinks': backlinks
-        }
+    # ret = {
+    #     'canvas': canvas_uri,
+    #     'curations_backlinks': backlinks_by_area
+    #     }
 
-    return jsonify(ret)
+    return jsonify(display_curation)
 
 if __name__ == '__main__':
     app.run(port=cfg.cfg['port'])
